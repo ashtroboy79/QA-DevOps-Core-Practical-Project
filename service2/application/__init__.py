025f3cad3b1cff378541cd0cdf7612ed6e8b198a
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-23 14:35:31 +0000

    AR:Started project setup

---
+++ service2/application/__init__.py
@@ -0,0 +1,4 @@
+from flask import flask
+from application import routes
+
+app = Flask(__name__)
---
+++ service2/application/routes.py
@@ -0,0 +1,6 @@
+from application import app
+from random import choice
+
+@app.route('/monster_species', methods=['Get'])
+def monster_species():
+    pass
