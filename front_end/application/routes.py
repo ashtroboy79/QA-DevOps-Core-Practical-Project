93481dfba28cca1bff0481e93f0d76ccde199949
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-27 20:59:48 +0000

    AR: Front end modified and tested

--- front_end/application/routes.py
+++ front_end/application/routes.py
@@ -4,7 +4,7 @@ import requests
 
 @app.route('/')
 def index():
-    monster_species = requests.get('http://service2:5000/monster_species').text
+    monster_species = requests.get('http://service2:5000/monster_species').json()
     monster_type = requests.get('http://service3:5000/monster_type').json()
-    encounter = requests.post('http://service4:5000/monster_class', json={"monster":monster_species, "type":monster_type['type']})
+    encounter = requests.post('http://service4:5000/monster_class', json={"monster":monster_species['monster'], "type":monster_type['type'], "minions":monster_species['minions'], "quantity":monster_type['quantity']})
     return render_template('index.html', encounter_text=encounter.text)
