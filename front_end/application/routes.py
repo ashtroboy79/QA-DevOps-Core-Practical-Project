ce8448d63bf0a531a5e8304f3c381d17d9b1892f
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-29 19:26:21 +0000

    AR: added call and test to external API

--- front_end/application/routes.py
+++ front_end/application/routes.py
@@ -4,7 +4,17 @@ import requests
 
 @app.route('/')
 def index():
+    minions = {
+        'Orcs' : 'orc',
+        'Skeletons' : 'skeleton',
+        'Goblins': 'goblin',
+        'Gnolls': 'gnoll',
+        'Dire Wolves' : 'dire-wolf'
+    }
     monster_species = requests.get('http://service2:5000/monster_species').json()
     monster_type = requests.get('http://service3:5000/monster_type').json()
     encounter = requests.post('http://service4:5000/monster_class', json={"monster":monster_species['monster'], "type":monster_type['type'], "minions":monster_species['minions'], "quantity":monster_type['quantity']})
-    return render_template('index.html', encounter_text=encounter.text)
+    minion = minions[monster_species['minions']]
+    minion_details = requests.get(f'https://www.dnd5eapi.co/api/monsters/{minion}/').json()
+    print(minion_details)
+    return render_template('index.html', encounter_text=encounter.text, minion_details=minion_details, minion=minion)
