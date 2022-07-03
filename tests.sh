46caccbe6d63385eaf8ee6ad9af73deebabbc7bd
Ahsan Rasul <77322613+ashtroboy79@users.noreply.github.com>
2022-07-03 14:05:37 +0100

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -4,7 +4,10 @@ declare -a dirs=(front_end monster_species monster_type monster_class)
 
 for dir in ${dirs[@]}; do
     cd ${dir}
+    python3 -m venv venv
+    source venv/bin/activate
     pip3 install -r requirements.txt
-    python3 -m pytest --cov=application --cov-report=html --cov-report term-missing
+    python3 -m pytest --cov=application --cov-report=html
+    deactivate
     cd ..
 done
