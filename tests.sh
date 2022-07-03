90a1d308f0faab05a83ea0ed9dbcf8eaad5aa3c8
ahsanrasul <rasul.ahsan@gmail.com>
2022-07-03 12:35:13 +0000

    AR: Trying to get jenkinsfile to run tests

--- tests.sh
+++ tests.sh
@@ -4,6 +4,8 @@ declare -a dirs=(front_end monster_species monster_type monster_class)
 
 for dir in ${dirs[@]}; do
     cd ${dir}
+    python3 -m venv venv
+    source venv/bin/activate
     pip3 install -r requirements.txt
     python3 -m pytest --cov=application --cov-report=html
     cd ..
